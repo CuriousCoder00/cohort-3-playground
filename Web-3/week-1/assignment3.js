b4e38fb1906210704724af1c09340c0b28f47c22
Kapil Jangid <kapiljangid040@gmail.com>
2024-11-12 08:49:28 +0530

    Add nonce-finding script for SHA-256 hash with specific prefix

---
+++ Web-3/week-1/assignment3.js
@@ -0,0 +1,26 @@
+/*
+What if I ask you to find a nonce for the following input - 
+
+sample input and output
+
+Input: harkirat => Raman | Rs 100 Ram => Ankit | Rs 101935085
+Output Hash: 00000780594bad6a25d5a240f9a3a399521979f1b320cd13658fe1232bf2d688
+*/
+
+const crypto = require("crypto");
+function findHashWithPrefix(prefix) {
+    let input = 0;
+    while (true) {
+        let inputStr = `harkirat => Raman | Rs 100 Ram => Ankit | Rs 101935085` + input.toString();
+        let hash = crypto.createHash("sha256").update(inputStr).digest("hex");
+        if (hash.startsWith(prefix)) {
+            return { input: inputStr, hash: hash };
+        }
+        input++;
+    }
+}
+
+// Find and print the input string and hash
+const result = findHashWithPrefix("00000");
+console.log(`Input: ${result.input}`);
+console.log(`Hash: ${result.hash}`);
